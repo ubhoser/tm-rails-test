f755affb8e61c54cb2c88a9e8b065c7c00539e67
Ümit Bedri Hoser <ubhoser@yahoo.com>
2013-12-14 23:12:06 +0100

    update Gemfile.lock and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130917065950) do
+ActiveRecord::Schema.define(:version => 20131214131716) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -41,16 +41,36 @@ ActiveRecord::Schema.define(:version => 20130917065950) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "role"
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "campaign_histories", :force => true do |t|
+    t.integer  "admin_user_id"
+    t.integer  "campaign_id"
+    t.integer  "budget"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
 
   create_table "campaigns", :force => true do |t|
     t.string   "name"
     t.integer  "budget"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "campaigns_platforms", :force => true do |t|
+    t.integer "campaign_id"
+    t.integer "platform_id"
+  end
+
+  create_table "platforms", :force => true do |t|
+    t.string   "operating_system"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
 end
