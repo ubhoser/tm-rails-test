63b15dee09fb05992df1eaa5eb8a2d33662a869f
Ümit Bedri Hoser <ubhoser@yahoo.com>
2013-12-14 23:05:57 +0100

    get current_user for CanCan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def access_denied(exception)
+    redirect_to admin_organizations_path, :alert => exception.message
+  end
+
+  # get current-user for CanCan
+  def current_user
+    current_admin_user
+  end
+
 end
