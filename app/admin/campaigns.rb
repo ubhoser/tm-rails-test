edc0d5eefa3855763e2d3b3615d68cddbab17e29
Ümit Bedri Hoser <ubhoser@yahoo.com>
2013-12-14 23:08:58 +0100

    part-3: compare the platfors and budget to create new Campaign-History

--- app/admin/campaigns.rb
+++ app/admin/campaigns.rb
@@ -1,8 +1,44 @@
 ActiveAdmin.register Campaign do
-  
+  controller do
+    before_filter :old_platform_ids, :only => [:new, :update]
+
+private
+    # Find the platform id's to compare with current-platform id's by update 
+    def old_platform_ids
+      begin
+        campaign = Campaign.find(params[:id])
+        @old_platform_ids = campaign.platforms.map{|p| p.id}
+      rescue
+        @old_platform_ids = []
+      end
+    end
+  end
+
   index do
+    column "platforms" do |c|
+      c.platforms.map{|p| p.operating_system}.join(', ')
+    end
     column :name
     column :budget
     actions
   end
+
+  form do |f|
+    f.inputs do
+      f.input :platforms, :as => :select, :collection => Platform.all.map{|p| [p.operating_system, p.id]}
+      f.input :name
+      f.input :budget
+    end
+    f.actions
+  end
+
+  # compare the platform id's and if changed and the current campaign has budget
+  # more than 1000 then create new CampaignHistory
+  after_save do |campaign|
+    @current_plaform_ids = campaign.platforms.map{|p| p.id}
+    if (@old_platform_ids != @current_plaform_ids && campaign.budget > 1000)
+      CampaignHistory.create({:campaign_id => campaign.id, :admin_user_id => current_admin_user.id, :budget => campaign.budget})
+    end
+  end
+
 end
