631d8bde477438c1ebd657d11880f4f8b616b08f
Ümit Bedri Hoser <ubhoser@yahoo.com>
2013-12-14 23:10:43 +0100

    add validaton and relation to platforms

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,12 @@
 class Campaign < ActiveRecord::Base
-  attr_accessible :name, :budget
 
+  has_and_belongs_to_many :platforms
+
+  attr_accessible :name, :budget, :platform_ids
+
+  validates :platforms, presence: true
   validates :name, presence: true
   validates :budget, numericality: { greater_than: 0 }
+
+
 end
