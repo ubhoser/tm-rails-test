c95e58a8f8ad0b686f683bc017a68de054607800
Ümit Bedri Hoser <ubhoser@yahoo.com>
2013-12-14 23:09:47 +0100

    new function to check user role

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,6 +6,11 @@ class AdminUser < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :role
   # attr_accessible :title, :body
+
+  def admin?
+    role == "admin"
+  end
+  
 end
