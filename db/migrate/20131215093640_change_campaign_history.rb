bfe87c4b1a58df0a2674a83c23463f377a0fe528
Ümit Bedri Hoser <ubhoser@yahoo.com>
2013-12-15 11:36:35 +0100

    correct the migration. Change rename_column to remove_column

--- db/migrate/20131215093640_change_campaign_history.rb
+++ db/migrate/20131215093640_change_campaign_history.rb
@@ -1,7 +1,7 @@
 class ChangeCampaignHistory < ActiveRecord::Migration
   def up
-    rename_column :campaign_histories, :campaign_id, :integer
-    rename_column :campaign_histories, :admin_user_id, :integer
+    remove_column :campaign_histories, :campaign_id
+    remove_column :campaign_histories, :admin_user_id
     add_column :campaign_histories, :campaign_name, :string
     add_column :campaign_histories, :admin_user_email, :string
     add_column :campaign_histories, :action, :string
@@ -9,10 +9,10 @@ class ChangeCampaignHistory < ActiveRecord::Migration
   end
 
   def down
-    rename_column :campaign_histories, :campaign_name, :string
-    rename_column :campaign_histories, :admin_user_email, :string
-    rename_column :campaign_histories, :action, :string
-    rename_column :campaign_histories, :message, :string
+    remove_column :campaign_histories, :campaign_name
+    remove_column :campaign_histories, :admin_user_email
+    remove_column :campaign_histories, :action
+    remove_column :campaign_histories, :message
     add_column :campaign_histories, :campaign_id, :integer
     add_column :campaign_histories, :admin_user_id, :integer
   end
